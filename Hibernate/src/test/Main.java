1d2b681f1ff2482b55cd4ff81ee1630e83aa7033
charliecooper45 <charliecooper98@gmail.com>
2014-12-20 17:32:01 +0000

    Mapping using xml

--- Hibernate/src/test/Main.java
+++ Hibernate/src/test/Main.java
@@ -1,9 +1,23 @@
 package test;
 
+import hibernate.classes.Student;
+
+import org.hibernate.Session;
+
+import utils.HibernateUtil;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Second Hello World");
-		System.out.println("Third Hello World");
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+        Student student = new Student();
+
+        student.setStudentName("JavaFun");
+        student.setStudentAge("19");
+
+        session.save(student);
+        session.getTransaction().commit();
+        System.out.println("Great! Student was saved");
 	}
 }
