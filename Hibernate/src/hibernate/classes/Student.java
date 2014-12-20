dda6ef4d6af149285bcf57aee42ca5fa82a01cf1
charliecooper45 <charliecooper98@gmail.com>
2014-12-20 17:43:19 +0000

    Mapping using annotations

--- Hibernate/src/hibernate/classes/Student.java
+++ Hibernate/src/hibernate/classes/Student.java
@@ -1,11 +1,28 @@
 package hibernate.classes;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "student", catalog = "hibernate_test_database")
 public class Student implements java.io.Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "STUDENT_ID", unique = true, nullable = false)
 	private Integer studentId;
+	
+	@Column(name = "STUDENT_NAME", nullable = false, length = 10)
 	private String studentName;
+	
+	@Column(name = "STUDENT_AGE", nullable = false, length = 20)
 	private String studentAge;
 
 	public Student() {
