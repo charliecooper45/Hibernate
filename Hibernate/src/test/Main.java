6e969b3a8c0193971a472f391da350c3eeee3a8a
charliecooper45 <charliecooper98@gmail.com>
2014-12-21 14:38:59 +0000

    c3p0 connection pooling

--- Hibernate/src/test/Main.java
+++ Hibernate/src/test/Main.java
@@ -11,15 +11,15 @@ public class Main {
 	public static void main(String[] args) {
         Session session = HibernateUtil.getSessionFactory().openSession();
 
-//        session.beginTransaction();
-//        Student student = new Student();
-//
-//        student.setStudentName("JavaFun");
-//        student.setStudentAge("19");
-//
-//        session.save(student);
-//        session.getTransaction().commit();
-//        System.out.println("Great! Student was saved");
+        session.beginTransaction();
+        Student student = new Student();
+
+        student.setStudentName("JavaFun");
+        student.setStudentAge("19");
+
+        session.save(student);
+        session.getTransaction().commit();
+        System.out.println("Great! Student was saved");
         
         Query namedQuery = session.getNamedQuery(Student.QUERY_BY_STUDENT_ID);
         namedQuery.setInteger("studentId", 1);
